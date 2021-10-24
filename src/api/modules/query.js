afcaaf224cfc72a0348e14315f5082da41299b8b
Kinza-ALi <kinzali.020@gmail.com>
2021-10-25 00:22:32 +0500

    Removed unnecessary code

--- src/api/modules/query.js
+++ src/api/modules/query.js
@@ -1,88 +1,65 @@
 import merge from "lodash.merge";
-
-export const controllers = {
-  createOne(model, body) {
-    return model.create(body);
-  },
-
-  deleteOne(docToDelete) {
-    return docToDelete.remove();
-  },
-
-  getOne(docToGet) {
-    return Promise.resolve(docToGet);
-  },
-
-  findByParam(model, id) {
-    return model.findById(id);
-  },
-};
+import { notFoundResponse, successResponsePost } from "./apiResponses.js";
 
 export const createOne = (model) => (req, res, next) => {
-  return controllers
-    .createOne(model, req.body)
-    .then((doc) => res.status(201).json(doc))
-    .catch((error) => next(error));
-};
+  let route = "post";
 
-export const updateOne = (model) => async (req, res, next) => {
-  model.findOneAndUpdate(
-    { _id: req.params.id },
-    req.body,
-    { new: true },
-    function (err, doc) {
-      if (err) res.send(err);
-      res.json(doc);
-    }
-  );
-};
-
-export const deleteOne = (model) => (req, res, next) => {
-  model.remove(
-    {
-      _id: req.params.id,
-    },
-    function (err, doc) {
-      if (err) res.send(err);
-      res.json({ message: " successfully deleted" });
-    }
-  );
-};
-
-export const getOne = (model) => (req, res, next) => {
-  model.findById(req.params.id, function (err, doc) {
-    if (err) res.send(err);
-    res.json(doc);
-  });
-};
-
-export const getAll = (model) => async (req, res, next) => {
-  let page = Number(req.query.page ? req.query.page : 1);
-  let perPage = Number(req.query.perPage ? req.query.perPage : 10);
-  let skipRecords = perPage * (page - 1);
-  let docs = await model.find({}).skip(skipRecords).limit(perPage);
-  return res.send(docs);
-};
-
-export const findByParam = (model) => (req, res, next, id) => {
-  return controllers
-    .findByParam(model, id)
+  return model
+    .create(req.body)
     .then((doc) => {
-      if (!doc) {
-        next(new Error("Not Found Error"));
-      } else {
-        req.docFromId;
-        next();
-      }
+      res.send(successResponsePost(doc, route));
     })
     .catch((error) => {
       next(error);
     });
 };
 
+export const updateOne = (model) => async (req, res, next) => {
+  let route = "update";
+  model.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true },
+    function (err, doc) {
+      console.log(err);
+      if (err || !doc) res.status(404).send(notFoundResponse());
+      res.send(successResponsePost(doc, route));
+    }
+  );
+};
+
+export const deleteOne = (model) => (req, res, next) => {
+  let route = "delete";
+  model.remove(
+    {
+      _id: req.params.id,
+    },
+    function (err, doc) {
+      if (err || !doc) res.status(404).send(notFoundResponse());
+      res.send(successResponsePost(doc, route));
+    }
+  );
+};
+
+export const getOne = (model) => (req, res, next) => {
+  let route = "get";
+  model.findById(req.params.id, function (err, doc) {
+    if (err || !doc) res.status(404).send(notFoundResponse());
+    res.send(successResponsePost(doc, route));
+  });
+};
+
+export const getAll = (model) => async (req, res, next) => {
+  let route = "get";
+  let page = Number(req.query.page ? req.query.page : 1);
+  let perPage = Number(req.query.perPage ? req.query.perPage : 10);
+  let skipRecords = perPage * (page - 1);
+  let docs = await model.find({}).skip(skipRecords).limit(perPage);
+  return res.send(successResponsePost(docs, route));
+};
+
 export const generateControllers = (model, overrides = {}) => {
   const defaults = {
-    findByParam: findByParam(model),
     getAll: getAll(model),
     getOne: getOne(model),
     deleteOne: deleteOne(model),
