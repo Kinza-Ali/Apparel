91bc3c3d34a7aff9f201d81d37537194cc79fe95
Kinza-Alii <kinza.ali@retailo.co>
2021-12-02 11:34:50 +0500

    Port replaced

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ mongoose
   })
   .then(() => console.log("Connected to Mongo .... "))
   .catch((error) => console.log(error.message));
-app.listen(process.env.PORT || 61500, () => {
+app.listen(3000, () => {
   console.log("connected to server");
 });
